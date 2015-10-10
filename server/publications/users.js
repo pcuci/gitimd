d29fb38e374d55c831c0892c54eaadb5217c8f10
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-10-11 00:40:05 +0300

    Profile edit done fix #16

---
+++ server/publications/users.js
@@ -0,0 +1,9 @@
+Meteor.publish(null, function () {
+    if (this.userId) {
+        return Meteor.users.find({_id:this.userId},{fields:{roles:1, services:1,
+                                                            emails:1, profile:1,
+                                                            username:1, keywords:1,
+                                                            createdAt:1
+                                                           }});
+    };
+});
