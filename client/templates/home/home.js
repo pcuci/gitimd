0250c4f2c7d3558fa9737c97beefcf45f0226482
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-10-11 17:16:44 +0300

    fixed teams subscription on home

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -4,6 +4,9 @@ Template.home.rendered = function() {
 
 
 Template.locateEvent.helpers({
+    teams: function () {
+	return Teams.find();
+    },
     membersCount: function () {
         return _.filter(Teams.findOne().members, function (el) {if (el.status!='applied') return true;}).length;
     },
@@ -11,3 +14,7 @@ Template.locateEvent.helpers({
         return _.filter(Teams.findOne().members, function (el) {if (el.status==='applied') return true;}).length;
     },
 });
+
+Template.locateEvent.rendered=function (){
+    Meteor.subscribe('teams');
+};
