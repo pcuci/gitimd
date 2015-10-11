038799821e4719854e0fba3efcef28ce6755cd5e
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-10-11 18:01:04 +0300

    add join to team & some roles staff, fixed #5

--- client/templates/teams/teams.js
+++ client/templates/teams/teams.js
@@ -17,8 +17,32 @@ AutoForm.addHooks(['editTeam'],{
 });
 
 Template.teams.helpers({
-    users: function () {
-	return Meteor.users.find();
+    usersMembers: function () {
+	var arr=Teams.findOne({_id:Router.current().params.slug});
+	if (arr && arr.members && (arr.members.length>0)) {
+	    arr=arr.members.map(function (el) {if (el.status!='applied') return el.id});
+	} else {
+	    arr=[];
+	};
+	return Meteor.users.find({_id: {$in:arr}});
+    },
+    usersWatchers: function () {
+	var arr=Teams.findOne({_id:Router.current().params.slug});
+	if (arr && arr.members && (arr.members.length>0)) {
+	    arr=arr.members.map(function (el) {if (el.status==='applied') return el.id});
+	} else {
+	    arr=[];
+	};
+	return Meteor.users.find({_id: {$in:arr}});
+    },
+    usersAll: function () {
+	var arr=Teams.findOne({_id:Router.current().params.slug});
+	if (arr && arr.members && (arr.members.length>0)) {
+	    arr=arr.members.map(function (el) {return el.id});
+	} else {
+	    arr=[];
+	};
+	return Meteor.users.find({_id: {$nin:arr}});
     },
     currentTeam: function () {
 	return Teams.findOne({_id: Router.current().params.slug});
@@ -35,10 +59,17 @@ Template.teams.helpers({
     },
     memberOf: function () {
 	return Teams.find({'members.id':this._id});
+    },
+    currentTeam: function () {
+	return Teams.findOne({_id: Router.current().params.slug});
+    },
+    mayEdit: function () {
+	return Teams.findOne({_id:Router.current().params.slug, 'members.id':Meteor.userId()});
     }
 });
  
 Template.teams.events({
-    'click .join': function () {
+    'click .joinTeam': function () {
+	Teams.update({_id:Router.current().params.slug},{$addToSet: {members:{id:Meteor.userId(), status:'applied'}}});
     }
 });
